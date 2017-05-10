0875fa8703589e2a17e6a99cf2ddf653a5124e97
douge <dougecox@gmail.com>
2017-05-10 11:51:26 -0700

    update

--- server/index.js
+++ server/index.js
@@ -56,6 +56,6 @@ request(recipeRetrievalOptions, function(error, response, body) {
 
 
 
-app.listen(3000, function() {
-  console.log("listening on port 3000")
+app.listen(process.env.PORT || 3000 , function() {
+  console.log(`istening on port ${process.evn.PORT}`)
 })
